943b3e195c756cd9b141f3f6e589528fe5606fd7
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-21 15:52:08 -0700

    Initial commit -Set up code sections

---
+++ countLetters.js
@@ -0,0 +1,19 @@
+// Support function(s)
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Main function
+const countLetters  = function(sentence) {
+  // Take in sentence as string
+
+  // Return object with the letters encountered as keys and their respective counts as values
+};
+
+
+// Test cases
