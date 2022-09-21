b2e9929d7d776e0841482e34702ef25b520dd92c
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-21 15:15:20 -0700

    Write countOnly function that takes in array of items and an object specifying which items to count Outputs object with items and respective count as key: value pairs

--- middle.js
+++ middle.js
@@ -24,23 +24,28 @@ const assertArraysEqual = function(array1, array2) {
 
 // Main function
 const middle = (array) => {
+  
   // Take in array as input
   // Create empty output array and a variable to hold the length of the input array
   let output = [];
   const inputLength = array.length;
+  
   // Arrays with zero/one/two elements have no middle, so return empty array
   if (array.length <= 2) {
     return output;
   }
+  
   // For arrays with even no. of elements, return two elements
   if (array.length % 2 === 0) {
     output.push(array[inputLength / 2 - 1], array[inputLength / 2]);
   } else {
     // For arrays with odd no. of elements, return one element
-    output.push(array[Math.floor(inputLength/2)]);
+    output.push(array[Math.floor(inputLength / 2)]);
   }
-  return output;  
-}
+  
+  return output;
+  
+};
 
 
 // Test cases
@@ -49,3 +54,4 @@ assertArraysEqual(middle([0]), []);
 assertArraysEqual(middle([0, 1]), []);
 assertArraysEqual(middle([0, 1, 2]), [1]);
 assertArraysEqual(middle(['a', 'b', 'c', 'd']), ['b', 'c']);
+assertArraysEqual(middle([NaN, NaN, undefined, null, NaN, NaN]), [undefined, null]);
