27a8c6a1e2adb8679ae17560076ebad67825dd95
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-20 20:21:36 -0700

    Update eqArrays.js function

--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
   for (let i = 0; i < arr1.length; i++) {
     // Compare arr1[i] to arr2[2]
     // If comparison evaluates to false, return false
@@ -19,6 +22,5 @@ const eqArrays = (arr1, arr2) => {
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// The following assertion passes because the eqArrays function assumes the two arrays passed in will always have the same length
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
