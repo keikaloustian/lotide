34e7829fe82274b8c1cccff24ef1c29e127065c5
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-21 17:26:07 -0700

    Finalize findKeyByValue function - Input: object to be searched and a search value - Output: the first key whose value matches the search value

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -10,6 +10,16 @@ const assertEqual = function(actual, expected) {
 
 // Main function
 const findKeyByValue = function(lookupObject, searchValue) {
+  
+  // Loop through object and check each key against the search value
+  for (let key in lookupObject) {
+    if (lookupObject[key] === searchValue) {
+      return key;
+    }
+  }
+  
+  // If no matches are found, no explicit returns needed
+  // If a function doesn't explicitly return anything, it'll output undefined by default
 
 };
 
@@ -24,6 +34,6 @@ const bestTVShowsByGenre = {
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), "undefined");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), undefined);
 assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
 
