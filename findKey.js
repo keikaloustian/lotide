f9ddaddd84ab440c965358a62048606a628a7441
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-22 16:26:35 -0700

    Initial commit

---
+++ findKey.js
@@ -0,0 +1,14 @@
+// Support function(s)
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Main function
+const findKey = function(object, callback) {
+  for ()
+}
