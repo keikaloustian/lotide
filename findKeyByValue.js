0d4c3ac69b9e091ca2e5ddd1525cab5777458919
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-21 17:09:12 -0700

    Initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// Support function(s)
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😊 Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Main function
+const findKeyByValue = function(lookupObject, searchValue) {
+
+};
+
+
+// Test cases
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), "undefined");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+
