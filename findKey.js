58864009aa4e23428b019837cca2ad72f240fd84
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-22 18:06:14 -0700

    Implement findKey function

--- findKey.js
+++ findKey.js
@@ -10,5 +10,37 @@ const assertEqual = function(actual, expected) {
 
 // Main function
 const findKey = function(object, callback) {
-  for ()
+
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+
 }
+
+
+// Test cases
+assertEqual(findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  }, x => x.stars === 3
+), 'Akaleri');
+
+assertEqual(findKey(
+  {
+    prop1: 1,
+    prop2: 2,
+    method1: () => {
+      console.log('This is method #1')
+    },
+    prop3: [1, 2, 4]
+  }, k => typeof k === 'function'
+), 'method1');
+
+
