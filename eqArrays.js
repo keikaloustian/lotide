d7ddb4f13c95a2270244c73647f142aff3b9a3fe
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-25 15:27:48 -0700

    Update - minor

--- eqArrays.js
+++ eqArrays.js
@@ -9,17 +9,17 @@ const eqArrays = (arr1, arr2) => {
   for (let i = 0; i < arr1.length; i++) {
     // Compare arr1[i] to arr2[2]
     // If comparison evaluates to false, return false
-    if (!(arr1[i] === arr2[i])) {
+    if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
 
-// // Tests
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// Tests
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 
 module.exports = eqArrays;
