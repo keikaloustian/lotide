cd21b6b9455879c4ffb37da22a7217f37a48bfc9
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-26 16:36:18 -0700

    Refactor index - minor

--- index.js
+++ index.js
@@ -1,38 +1,20 @@
-const assertArraysEqual = require('./assertArraysEqual');
-const assertEqual = require('./assertEqual');
-const assertObjectsEqual = require('./assertObjectsEqual');
-const countLetters = require('./countLetters');
-const countOnly = require('./countOnly');
-const eqArrays = require('./eqArrays');
-const eqObjects = require('./eqObjects');
-const findKey = require('./findKey');
-const findKeyByValue = require('./findKeyByValue');
-const flatten = require('./flatten');
-const head   = require('./head');
-const letterPositions = require('./letterPositions');
-const map = require('./map');
-const middle = require('./middle');
-const tail   = require('./tail');
-const takeUntil = require('./takeUntil');
-const without = require('./without');
-
-
 module.exports = {
-  assertArraysEqual,
-  assertEqual,
-  assertObjectsEqual,
-  countLetters,
-  countOnly,
-  eqArrays,
-  eqObjects,
-  findKey,
-  findKeyByValue,
-  flatten,
-  head,
-  letterPositions,
-  map,
-  middle,
-  tail,
-  takeUntil,
-  without
+  assertArraysEqual: require('./assertArraysEqual'),
+  assertEqual: require('./assertEqual'),
+  assertObjectsEqual: require('./assertObjectsEqual'),
+  countLetters: require('./countLetters'),
+  countOnly: require('./countOnly'),
+  eqArrays: require('./eqArrays'),
+  eqObjects: require('./eqObjects'),
+  findKey: require('./findKey'),
+  findKeyByValue: require('./findKeyByValue'),
+  flatten: require('./flatten'),
+  head: require('./head'),
+  letterPositions: require('./letterPositions'),
+  map: require('./map'),
+  middle: require('./middle'),
+  tail: require('./tail'),
+  takeUntil: require('./takeUntil'),
+  without: require('./without'),
 };
+
