bc8e185552f51ea7ec0e499a802ee4cf9137e6ab
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-26 13:57:10 -0700

    Refactor middleTest to use mocha & chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,30 @@
-// Support function(s)
-const assertArraysEqual = require('../assertArraysEqual');
-
-// Main function
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// Test cases
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([0]), []);
-assertArraysEqual(middle([0, 1]), []);
-assertArraysEqual(middle([0, 1, 2]), [1]);
-assertArraysEqual(middle(['a', 'b', 'c', 'd']), ['b', 'c']);
-assertArraysEqual(middle([NaN, NaN, undefined, null, NaN, NaN]), [undefined, null]);
+describe("#middle", () => {
+  
+  it("returns [] for [0]", () => {
+    assert.deepEqual(middle([0]), []);
+  });
+
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+
+  it("returns [] for [0, 1]", () => {
+    assert.deepEqual(middle([0, 1]), []);
+  });
+
+  it("returns [1] for [0, 1, 2]", () => {
+    assert.deepEqual(middle([0, 1, 2]), [1]);
+  });
+
+  it("returns ['b', 'c'] for ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(middle(['a', 'b', 'c', 'd']), ['b', 'c']);
+  });
+
+  it("returns [undefined, null] for [[NaN, NaN, undefined, null, NaN, NaN]", () => {
+    assert.deepEqual(middle([NaN, NaN, undefined, null, NaN, NaN]), [undefined, null]);
+  });
+
+});
