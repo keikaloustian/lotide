6972a77ebbb673de6e3f63a70f8c08b7962d5a22
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-26 09:54:31 -0700

    Refactor eqArrays for modularity and separate tests into file in subdir

--- eqArrays.js
+++ eqArrays.js
@@ -1,7 +1,3 @@
-// Support function
-const assertEqual = require('./assertEqual.js');
-
-// Main function
 const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
@@ -22,17 +18,6 @@ const eqArrays = (arr1, arr2) => {
 
   return true;
 };
-
-// // Tests
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-// assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-// assertEqual(eqArrays([[2, 3], [4, [1, [2, [3]]]]], [[2, 3], [4, [1, [2, [3]]]]]), true);
-
 
 
 module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,16 @@
+// Support function(s)
+const assertEqual = require('../assertEqual');
+
+// Main function
+const eqArrays = require('../eqArrays');
+
+
+// Tests
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+assertEqual(eqArrays([[2, 3], [4, [1, [2, [3]]]]], [[2, 3], [4, [1, [2, [3]]]]]), true);
