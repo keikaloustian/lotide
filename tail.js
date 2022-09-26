590aa9f85469a4408b52671e6fa9fe059907f79d
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-26 09:43:23 -0700

    Refactor tail for modularity and separate tests

--- tail.js
+++ tail.js
@@ -1,21 +1,6 @@
-// Support function(s)
-const assertEqual = require('./assertEqual.js');
-
-
-//Main function
 const tail = (array) => {
   return array.slice(1);
 };
-
-
-// // Tests
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words);
-// assertEqual(words.length, 3);
-
-// console.log(tail([1, 2, 3, 4, 5]));
-// console.log(tail([1]));
-// console.log(tail([]));
 
 
 module.exports = tail;
