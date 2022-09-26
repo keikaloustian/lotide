fe87e9a446d347f214dec8667d781dc2527a4f16
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-25 17:26:17 -0700

    Implement recursive capability to eqArray so it can handle nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -6,13 +6,20 @@ const eqArrays = (arr1, arr2) => {
   if (arr1.length !== arr2.length) {
     return false;
   }
+
   for (let i = 0; i < arr1.length; i++) {
-    // Compare arr1[i] to arr2[2]
-    // If comparison evaluates to false, return false
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+      continue;
+    }
+    
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
+
   return true;
 };
 
@@ -21,5 +28,11 @@ const eqArrays = (arr1, arr2) => {
 // assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 // assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
 // assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+// assertEqual(eqArrays([[2, 3], [4, [1, [2, [3]]]]], [[2, 3], [4, [1, [2, [3]]]]]), true);
+
+
 
 module.exports = eqArrays;
