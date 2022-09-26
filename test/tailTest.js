d72f98d9109ca4802bb123b7cd74b3f15c44163c
Kei Kaloustian <kei.kaloustian@gmail.com>
2022-09-26 09:44:00 -0700

    Separate tests from tail.js into file in subdirectory

---
+++ test/tailTest.js
@@ -0,0 +1,14 @@
+// Support function(s)
+const assertEqual = require('../assertEqual.js');
+
+//Main function
+const tail = require('../tail');
+
+// Tests
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
+
+console.log(tail([1, 2, 3, 4, 5]));
+console.log(tail([1]));
+console.log(tail([]));
